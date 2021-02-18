479d19c851c0d925f227564685b034c9dec8a430
Sophia <suna9267@gmail.com>
2021-02-18 17:36:35 +0900

    Update ex1-10.js
    ex1-10

---
+++ ex1-10.js
@@ -0,0 +1,21 @@
+let user = {
+    name: 'Jaenam',
+    gender: 'male'
+};
+
+let changeName = function (user, newName) {
+    var newUser = user;
+    newUser.name = newName;
+    return newUser;
+};
+
+let user2 = changeName(user, 'Jung');
+
+if (user != user2) {
+    console.log('유저 정보가 변경되었습니다.');
+}
+
+console.log(user.name, user2.name);
+console.log(user === user2);
+
+
